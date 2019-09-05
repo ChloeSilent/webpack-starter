b04e214bdf904606780b3fe92af5ee9aea7605f5
OlgaShitova <os@3d.ru>
2019-09-05 19:15:51 +0300

    webpack-starter

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// если запусить тpm run dev - будет рендер проекта из памяти,
+// если запустить npm run build - будет создана папка dist
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {sourceMap: true}
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {sourceMap: true, config: {path: 'postcss.config.js' }}
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {sourceMap: true}
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {sourceMap: true}
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {sourceMap: true, config: {path: 'postcss.config.js' }}
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        overlay: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+}
